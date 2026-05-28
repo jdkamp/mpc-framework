e551b16e1011f3a7bb2bb63ced15ba03237f7405
jdkamp <205521783+jdkamp@users.noreply.github.com>
2026-05-28 15:58:11 +0200

    Add GoogleTest and BicycleModel unit tests
    - Integrate GoogleTest
    - Add tests/BicycleModelTest.cpp containing unit tests that verify BicycleModel dynamics, state/input dimensions, and Jacobian shapes/values to validate model behavior

---
+++ tests/BicycleModelTest.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+#include "models/BicycleModel.hpp"
+#include <cmath>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+class BicycleModelTest : public ::testing::Test {
+protected:
+    BicycleModel model{2.7};
+};
+
+TEST_F(BicycleModelTest, DynamicsStraightLine) {
+    VectorXd x(4);
+    x << 0, 0, 0, 1;  // px, py, psi, vel
+
+    VectorXd u(2);
+    u << 0, 0;  // delta, a
+
+    VectorXd xdot = model.dynamics(x, u);
+
+    EXPECT_NEAR(xdot(0), 1, 1e-6);
+    EXPECT_NEAR(xdot(1), 0, 1e-6);
+    EXPECT_NEAR(xdot(2), 0, 1e-6);
+    EXPECT_NEAR(xdot(3), 0, 1e-6);
+}
+
+TEST_F(BicycleModelTest, DynamicsCurveDriving) {
+    VectorXd x(4);
+    x << 0, 0, M_PI/2, 1;  // px, py, psi, vel
+
+    VectorXd u(2);
+    u << 0, 0;  // delta, a
+
+    VectorXd xdot = model.dynamics(x, u);
+
+    EXPECT_NEAR(xdot(0), 0, 1e-6);
+    EXPECT_NEAR(xdot(1), 1, 1e-6);
+    EXPECT_NEAR(xdot(2), 0, 1e-6);
+    EXPECT_NEAR(xdot(3), 0, 1e-6);
+}
+
+TEST_F(BicycleModelTest, StateDimension) {
+    EXPECT_EQ(model.state_dim(), 4);
+}
+
+TEST_F(BicycleModelTest, InputDimension) {
+    EXPECT_EQ(model.input_dim(), 2);
+}
+
+TEST_F(BicycleModelTest, JacobianDimension) {
+    VectorXd x(4);
+    x << 0, 0, 0, 1;  // px, py, psi, vel
+
+    VectorXd u(2);
+    u << 0, 0;  // delta, a
+
+    MatrixXd A = model.jacobian_x(x, u);
+    EXPECT_EQ(A.rows(), 4);
+    EXPECT_EQ(A.cols(), 4);
+
+    MatrixXd B = model.jacobian_u(x, u);
+    EXPECT_EQ(B.rows(), 4);
+    EXPECT_EQ(B.cols(), 2);
+}
+
+TEST_F(BicycleModelTest, JacobianXValues) {
+    VectorXd x(4);
+    x << 0, 0, 0, 1;  // px, py, psi, vel
+
+    VectorXd u(2);
+    u << 0, 0;  // delta, a
+
+    MatrixXd A = model.jacobian_x(x, u);
+
+    EXPECT_NEAR(A(0,2), 0, 1e-6);
+    EXPECT_NEAR(A(0,3), 1, 1e-6);
+    EXPECT_NEAR(A(1,2), 1, 1e-6);
+    EXPECT_NEAR(A(1,3), 0, 1e-6);
+    EXPECT_NEAR(A(2,3), 0, 1e-6);
+}
+
+TEST_F(BicycleModelTest, JacobianUValues) {
+    VectorXd x(4);
+    x << 0, 0, 0, 1;  // px, py, psi, vel
+
+    VectorXd u(2);
+    u << 0, 0;  // delta, a
+
+    MatrixXd B = model.jacobian_u(x, u);
+    EXPECT_NEAR(B(2,0), 1/2.7, 1e-6);
+    EXPECT_NEAR(B(3,1), 1, 1e-6);
+
+}
